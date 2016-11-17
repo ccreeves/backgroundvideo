31551c9701d0e8ca765425da1ec77605e74989c6
ccreeves <reeves_chris@ymail.com>
2016-11-17 01:17:20 -0800

    Update backgroundvideo.m

--- src/ios/backgroundvideo.m
+++ src/ios/backgroundvideo.m
@@ -51,7 +51,7 @@
 
     //Capture session
     session = [[AVCaptureSession alloc] init];
-    [session setSessionPreset:AVCaptureSessionPresetPhoto];
+    [session setSessionPreset:AVCaptureSessionPresetHigh];
 
     //Get the front camera and set the capture device
     AVCaptureDevice *inputDevice = [self getCamera: self.camera];
